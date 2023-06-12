4d8393e1ed97a7d8edfe972d9b4fab5393e4f4bf
Joe Tsai <tzy0204@gmai.com>
2023-06-12 12:14:36 +0800

    066 no answer

---
+++ 0066/66.加一.cpp
@@ -0,0 +1,25 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem66.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=66 lang=cpp
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        int result = 100;
+        return digits;
+    }
+};
+// @lc code=end
+
