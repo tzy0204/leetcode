588f725e39f8fe02a370ba07d2fe33e347f1c259
Joe Tsai <tzy0204@gmail.com>
2023-06-17 21:09:00 +0800

    our runtime beats 100 % of cpp submissions

--- 0066/66.加一.cpp
+++ 0066/66.加一.cpp
@@ -1,23 +1,39 @@
-// @before-stub-for-debug-begin
-#include <vector>
-#include <string>
-#include "commoncppproblem66.h"
-
-using namespace std;
-// @before-stub-for-debug-end
-
 /*
  * @lc app=leetcode.cn id=66 lang=cpp
  *
  * [66] 加一
  */
-
+#include <vector>
+using namespace std;
 // @lc code=start
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        // result 
+        // [1,9,0,0,0]
+        //             i
+        // [1,8,9,9,9],  0
+        // [1,8,9,9,0],  1
+        // [1,8,9,0,0],  2
+        // [1,9,0,0,0],  3
+        // [1,9,0,0,0],  4
+        vector<int> result;
+        for (auto it = digits.rbegin(); it != digits.rend();it++) {
+            if (*it == 9) {
+                *it = 0;
+            }
+            else {
+                *it +=1;
+                break;
+            }
+        }
 
-        int result = 100;
+        // pos process for [9,9,9,...] input
+        if (digits[0] == 0) {
+            vector<int> result(digits.size(),0);
+            result.insert(result.begin(), 1);
+            return result;
+        }
         return digits;
     }
 };
