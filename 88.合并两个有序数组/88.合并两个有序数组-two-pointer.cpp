dd82f58797c312e58dfbef27d8fcc12b2c30f02a
Joe Tsai <tzy0204@gmai.com>
2023-07-27 16:26:55 +0800

    ref - two pointer

---
+++ 88.合并两个有序数组/88.合并两个有序数组-two-pointer.cpp
@@ -0,0 +1,58 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem88.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=88 lang=cpp
+ * @lcpr version=21912
+ *
+ * [88] 合并两个有序数组
+ */
+#include <vector>
+using namespace std;
+// @lc code=start
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--; k--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--; k--;
+            }
+        }
+        while(j>=0){
+            nums1[k] = nums2[j];
+            j--; k--;
+        }
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,0,0,0]\n3\n[2,5,6]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n[]\n0\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n0\n[1]\n1\n
+// @lcpr case=end
+
+ */
+
