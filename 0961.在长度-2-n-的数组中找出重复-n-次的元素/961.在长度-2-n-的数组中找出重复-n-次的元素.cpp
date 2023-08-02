ef98dfca098afd1ca1c976b400ece2385b05b47c
Joe Tsai <tzy0204@gmai.com>
2023-08-02 17:29:15 +0800

    unorder_map + different value check, faster soluton

--- 0961.在长度-2-n-的数组中找出重复-n-次的元素/961.在长度-2-n-的数组中找出重复-n-次的元素.cpp
+++ 0961.在长度-2-n-的数组中找出重复-n-次的元素/961.在长度-2-n-的数组中找出重复-n-次的元素.cpp
@@ -11,13 +11,18 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         unordered_map<int,int> map;
-        int res;
+        int res = -1;
         for (int i = 0; i < nums.size(); i++) {
+            // nums 由 n + 1 个 不同的 元素组成，且其中一个元素恰好重复 n 次
+            //                  ^^^^^^^^^^^^^
+            // 表示其他數字不會重複，所以只要重複就是result
+            if (map[nums[i]] != 0)return nums[i];
             map[nums[i]] +=1;
-            if (map[nums[i]] == (nums.size() / 2)) {
-                res = nums[i];
-                break;
-            }
+            // map[nums[i]] +=1;
+            // if (map[nums[i]] == (nums.size() / 2)) {
+            //     res = nums[i];
+            //     break;
+            // }
         }
         return res;
     }
